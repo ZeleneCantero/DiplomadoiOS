2dcd32dddcc619950cab8a623db8c7b6363aa5ed
ZeleneCantero <zeleneisayanacantero@gmail.com>
2025-02-21 18:00:01 -0600

    Agregando primer app

---
+++ Primer app/IAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Diplomado on 21/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
